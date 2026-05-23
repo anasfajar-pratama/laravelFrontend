b3f4cc1adee363fe7d506d08379f3237d4b81c3e
anasfajar <simpirnimil@gmail.com>
2026-05-23 09:14:53 +0700

    pertemuan11

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,6 +4,9 @@ import Register from '../pages/Register';
 import Dashboard from '../pages/Dashboard';
 import ProdukList from '../pages/ProdukList';
 import POSPage from '../pages/POSPage';
+import PesananPage from '../pages/PesananPage';
+import PelangganPage from '../pages/PelangganPage';
+
 
 function MainRoutes() {
   return (
@@ -14,6 +17,8 @@ function MainRoutes() {
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/produk" element={<ProdukList />} />
       <Route path="/pos" element={<POSPage />} />
+      <Route path="/pesanan" element={<PesananPage />} />
+      <Route path="/pelanggan" element={<PelangganPage />} />
       
     </Routes>
   );
